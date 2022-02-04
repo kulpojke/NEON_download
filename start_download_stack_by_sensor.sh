df4bf637189950a51f45885bf98ca9fd41e1d8b1
michael <michaelhuggins@protonmail.com>
2022-02-04 11:35:49 -0800

    renamed stuff

--- start_download_stack_by_sensor.sh
+++ start_download_stack_by_sensor.sh
@@ -3,7 +3,7 @@
 while getopts ":h" option; do
    case $option in
       h) # display Help
-        echo "SYNOPSIS"  
+        echo "SYNOPSIS"
         echo "     has args"
         echo "DESCRIPTION"
         echo "     does things"
@@ -17,6 +17,6 @@ STARTDATE=$3
 ENDDATE=$4
 APITOKEN=$5
 
-docker run --rm -it -v $SAVEPATH:/out --user $(id -u):$(id -g) quay.io/kulpojke/neon-timeseries:ts_construct-0d0154b $SITE $STARTDATE $ENDDATE $APITOKEN 
+docker run --rm -it -v $SAVEPATH:/out --user $(id -u):$(id -g) quay.io/kulpojke/neon-timeseries:ts_construct-0d0154b $SITE $STARTDATE $ENDDATE $APITOKEN
 
 # ./start_download.sh /media/data/Downloads/neon SOAP 2020-01 2020-07 $TOKEN
